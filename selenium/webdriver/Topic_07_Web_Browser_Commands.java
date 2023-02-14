fb14f01899395915f3cd54bf4a6fd052c654119c
Tran Anh Kiet <takiet197@gmail.com>
2023-02-14 23:42:37 +0700

    Topic 20 (WebBrowser Commands) ver2

--- selenium/webdriver/Topic_07_Web_Browser_Commands.java
+++ selenium/webdriver/Topic_07_Web_Browser_Commands.java
@@ -1,10 +1,11 @@
 package webdriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.logging.LogType;
 import org.testng.Assert;
@@ -33,7 +34,7 @@ public class Topic_07_Web_Browser_Commands {
 	}
 
 	@Test
-	public void TC_01_Browser() {
+	public void TC_01_Browser() throws MalformedURLException {
 		driver.close();
 		driver.quit();
 		driver.findElement(By.id(""));
@@ -47,10 +48,27 @@ public class Topic_07_Web_Browser_Commands {
 		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
 		driver.manage().timeouts().setScriptTimeout(15, TimeUnit.SECONDS);
-		driver.manage().window().setSize(null);
+		
+		org.openqa.selenium.Point point = driver.manage().window().getPosition();
+		point.getX();
+		point.getY();
+		driver.manage().window().setPosition(new org.openqa.selenium.Point(1990,1987));
+		
+		Dimension dimension = driver.manage().window().getSize();
+		dimension.getHeight();
+		dimension.getWidth();
+		driver.manage().window().setSize(new Dimension(1366, 768));
 		
 		String currentUrl = driver.getCurrentUrl();
 		Assert.assertEquals(currentUrl, "https://google.com");
+		
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		driver.navigate().to("");
+		driver.navigate().to(new URL(""));
+		
+		driver.switchTo().alert();
 	}
 
 	@Test
