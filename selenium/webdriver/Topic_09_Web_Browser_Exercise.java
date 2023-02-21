44971cfe63f334ba1bc94970c5df2479d8e259aa
Tran Anh Kiet <takiet197@gmail.com>
2023-02-21 23:41:48 +0700

    Topic 22 (WebBrowser/ WebElement Excercise)

---
+++ selenium/webdriver/Topic_09_Web_Browser_Exercise.java
@@ -0,0 +1,89 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_09_Web_Browser_Exercise {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+			// Window
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		} else {
+			// MAC OS 
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+		}
+
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+	
+	@Test
+	public void TC_01_Url() {
+		driver.get("http://live.techpanda.org/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+		Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+		
+	}
+	
+	@Test
+	public void TC_02_Title() {
+		driver.get("http://live.techpanda.org/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		sleepInSecond(2);
+		Assert.assertEquals(driver.getTitle(), "Customer Login");
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+		sleepInSecond(2);
+		Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
+	}
+	
+	@Test
+	public void TC_03_Navigation() {
+		driver.get("http://live.techpanda.org/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+		Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/create/");
+		driver.navigate().back();
+		Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/customer/account/login/");
+		driver.navigate().forward();
+		Assert.assertEquals(driver.getTitle(), "Create New Customer Account");
+	}
+	
+	@Test
+	public void TC_04_Page_Source() {
+		driver.get("http://live.techpanda.org/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		Assert.assertTrue(driver.getPageSource().contains("Login or Create an Account"));
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+		Assert.assertTrue(driver.getPageSource().contains("Create an Account"));
+	}
+	
+	public void sleepInSecond(long timeout) {
+		try {
+			Thread.sleep(timeout*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
---
+++ selenium/webdriver/Topic_10_Web_Element_Exercise.java
@@ -0,0 +1,198 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_10_Web_Element_Exercise {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
+
+	@BeforeClass
+	public void beforeClass() {
+		if (osName.contains("Windows")) {
+			// Window
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+		} else {
+			// MAC OS 
+			System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+		}
+
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+	
+	@Test
+	public void TC_01_Displayed() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		if (driver.findElement(By.id("email")).isDisplayed()) {
+			System.out.println("Email Textbox is displayed");
+			driver.findElement(By.id("email")).sendKeys("Automation Testing");
+		} else {
+			System.out.println("Email Textbox is not displayed");
+		}
+		
+		if (driver.findElement(By.id("under_18")).isDisplayed()) {
+			System.out.println("Age under 18 is displayed");
+			driver.findElement(By.id("under_18")).click();
+		} else {
+			System.out.println("Age under 18 is not displayed");
+		}
+		
+		if (driver.findElement(By.id("edu")).isDisplayed()) {
+			System.out.println("Education Textarea is displayed");
+			driver.findElement(By.id("edu")).sendKeys("Automation Testing");
+		} else {
+			System.out.println("Education Textarea is not displayed");
+		}
+		
+		if(driver.findElement(By.xpath("//h5[text()='Name: User5']")).isDisplayed()) {
+			System.out.println("Name: User5 is displayed");
+		} else {
+			System.out.println("Name: User5 is not displayed");
+		}
+	}
+	
+	@Test
+	public void TC_02_Enabled() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		
+		if (driver.findElement(By.id("disable_password")).isEnabled()) {
+			System.out.println("Password Textbox is enabled");
+		} else {
+			System.out.println("Password Textbox is disabled");
+		}
+		
+		if (driver.findElement(By.id("mail")).isEnabled()) {
+			System.out.println("Email Textbox is enabled");
+		} else {
+			System.out.println("Email Textbox is disabled");
+		}
+		
+		if (driver.findElement(By.id("radio-disabled")).isEnabled()) {
+			System.out.println("Radio Button is enabled");
+		} else {
+			System.out.println("Radio Button is disabled");
+		}
+		
+		if (driver.findElement(By.id("development")).isEnabled()) {
+			System.out.println("Development Checkbox is enabled");
+		} else {
+			System.out.println("Development Checkbox is disabled");
+		}
+	}
+	
+	@Test
+	public void TC_03_Selected() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		
+		driver.findElement(By.id("under_18")).click();
+		driver.findElement(By.id("java")).click();
+		
+		Assert.assertTrue(driver.findElement(By.id("under_18")).isSelected());
+		Assert.assertTrue(driver.findElement(By.id("java")).isSelected());
+
+		driver.findElement(By.id("java")).click();
+		
+		Assert.assertTrue(!driver.findElement(By.id("java")).isSelected());
+	}
+	
+	@Test
+	public void TC_04_Mailchimp() {
+		driver.get("https://login.mailchimp.com/signup");
+		
+		driver.findElement(By.xpath("//div[@id='onetrust-close-btn-container']//button[@aria-label='Close']")).click();
+		
+		driver.findElement(By.id("email")).sendKeys("Automation@gmail.com");
+		driver.findElement(By.id("new_password")).sendKeys("");
+		driver.findElement(By.id("create-account-enabled")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='lowercase-char not-completed']/span")).getText(), "One lowercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='uppercase-char not-completed']/span")).getText(), "One uppercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='number-char not-completed']/span")).getText(), "One number");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='special-char not-completed']/span")).getText(), "One special character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='8-char not-completed']/span")).getText(), "8 characters minimum");
+		
+		driver.findElement(By.id("new_password")).clear();
+		driver.findElement(By.id("new_password")).sendKeys("aaa");
+		driver.findElement(By.id("create-account-enabled")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='lowercase-char completed']/span")).getText(), "One lowercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='uppercase-char not-completed']/span")).getText(), "One uppercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='number-char not-completed']/span")).getText(), "One number");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='special-char not-completed']/span")).getText(), "One special character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='8-char not-completed']/span")).getText(), "8 characters minimum");
+		
+		driver.findElement(By.id("new_password")).clear();
+		driver.findElement(By.id("new_password")).sendKeys("AAA");
+		driver.findElement(By.id("create-account-enabled")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='lowercase-char not-completed']/span")).getText(), "One lowercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='uppercase-char completed']/span")).getText(), "One uppercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='number-char not-completed']/span")).getText(), "One number");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='special-char not-completed']/span")).getText(), "One special character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='8-char not-completed']/span")).getText(), "8 characters minimum");
+		
+		driver.findElement(By.id("new_password")).clear();
+		driver.findElement(By.id("new_password")).sendKeys("123");
+		driver.findElement(By.id("create-account-enabled")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='lowercase-char not-completed']/span")).getText(), "One lowercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='uppercase-char not-completed']/span")).getText(), "One uppercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='number-char completed']/span")).getText(), "One number");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='special-char not-completed']/span")).getText(), "One special character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='8-char not-completed']/span")).getText(), "8 characters minimum");
+		
+		driver.findElement(By.id("new_password")).clear();
+		driver.findElement(By.id("new_password")).sendKeys("#@$");
+		driver.findElement(By.id("create-account-enabled")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='lowercase-char not-completed']/span")).getText(), "One lowercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='uppercase-char not-completed']/span")).getText(), "One uppercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='number-char not-completed']/span")).getText(), "One number");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='special-char completed']/span")).getText(), "One special character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='8-char not-completed']/span")).getText(), "8 characters minimum");
+	
+		driver.findElement(By.id("new_password")).clear();
+		driver.findElement(By.id("new_password")).sendKeys("12345678");
+		driver.findElement(By.id("create-account-enabled")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='lowercase-char not-completed']/span")).getText(), "One lowercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='uppercase-char not-completed']/span")).getText(), "One uppercase character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='number-char completed']/span")).getText(), "One number");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='special-char not-completed']/span")).getText(), "One special character");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='8-char completed']/span")).getText(), "8 characters minimum");
+		
+		driver.findElement(By.id("new_password")).clear();
+		driver.findElement(By.id("new_password")).sendKeys("Q123ab@#$");
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='lowercase-char completed']/span")).getText(), "");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='uppercase-char completed']/span")).getText(), "");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='number-char completed']/span")).getText(), "");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='special-char completed']/span")).getText(), "");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='8-char completed']/span")).getText(), "");
+	}
+	
+	public void sleepInSecond(long timeout) {
+		try {
+			Thread.sleep(timeout*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
