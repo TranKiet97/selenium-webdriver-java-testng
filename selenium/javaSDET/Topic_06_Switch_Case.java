f6b80a44fc8b2f6646bdb89c24bb6d4434308ace
Tran_Anh_Kiet <takiet197@gmail.com>
2023-03-01 17:39:53 +0700

    Topic 39 (Java Basic - Part IV: Switch-Case/ For/ ForEach)

---
+++ selenium/javaSDET/Topic_06_Switch_Case.java
@@ -0,0 +1,36 @@
+package javaSDET;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+
+public class Topic_06_Switch_Case {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	
+	@Parameters("browser")
+	public void TC_01_Switch_Case(String browserName) {
+		
+		switch (browserName) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+		case "edge":
+			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+			break;
+			
+		default:
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			break;
+		}
+		
+		System.out.println(browserName);
+		System.out.println(driver.toString());
+	}
+
+}
---
+++ selenium/javaSDET/Topic_07_For_ForEach.java
@@ -0,0 +1,37 @@
+package javaSDET;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Topic_07_For_ForEach {
+	WebDriver driver;
+	
+	@Test
+	public void TC_01_For() {
+		for (int i = 0; i <= 9; i++) {
+			System.out.println(i);
+		}
+	}
+	
+	@Test
+	public void TC_02_ForEach() {
+		String[] cityName = {"Ha Noi", "Ho Chi Minh", "Da Nang", "Can Tho"};
+		
+		List<String> cityAddress = new ArrayList<String>();
+		
+		System.out.println(cityAddress.size());
+		
+		for (String city : cityName) {
+			cityAddress.add(city);
+		}
+		
+		System.out.println(cityAddress.size());
+		
+		for (String cityAdd : cityAddress) {
+			System.out.println(cityAdd);
+		}
+	}
+}
