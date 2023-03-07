8b5ca2f88899314fffd2db8359bbdc61b138226f
Tran_Anh_Kiet <takiet197@gmail.com>
2023-03-07 12:48:27 +0700

    Merge branch 'master' of https://github.com/TranKiet97/selenium-webdriver-java-testng.git

--- selenium/webdriver/Topic_17_Alert.java
+++ selenium/webdriver/Topic_17_Alert.java
@@ -14,7 +14,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_16_Alert {
+public class Topic_17_Alert {
 	WebDriver driver;
 	WebDriverWait explicitWait;
 	Alert alert;
