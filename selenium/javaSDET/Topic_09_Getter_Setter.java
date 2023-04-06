d86e34d311c4644b60b5da6d9f8ee6c6f4bcbe0b
Tran Anh Kiet <takiet197@gmail.com>
2023-04-06 11:06:24 +0700

    Topic 58 (Java Basic - Part VI: Java Keyword/ Convention) & Topic 59 (Java Basic - Part VII: While/ Do-While/ Array)

---
+++ selenium/javaSDET/Topic_09_Getter_Setter.java
@@ -0,0 +1,15 @@
+package javaSDET;
+
+public class Topic_09_Getter_Setter {
+	
+	private String carName;
+	
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+	
+	public String getCarName() {
+		return carName;
+	}
+	
+}
---
+++ selenium/javaSDET/Topic_10_Access_Modifier.java
@@ -0,0 +1,12 @@
+package javaSDET;
+
+public class Topic_10_Access_Modifier {
+
+	public static void main(String[] args) {
+		Topic_09_Getter_Setter topic_09 = new Topic_09_Getter_Setter();
+		topic_09.setCarName("Toyota");
+		String newCar = topic_09.getCarName();
+		System.out.println(newCar);
+	}
+
+}
